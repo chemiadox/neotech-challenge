ffead7d7ca1f7c46a82491834dbc40d22125539c
chemiadox <nt.xine@gmail.com>
2023-11-18 14:21:38 +0100

    remove useless config lib

--- apps/wallet-api/src/main.ts
+++ apps/wallet-api/src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { WalletApiModule } from './wallet-api.module';
-import { SharedConfigService } from '@app/shared-config';
 
 async function bootstrap() {
-  const configService = new SharedConfigService();
-  const config = configService.getConfig();
-
   const app = await NestFactory.create(WalletApiModule);
-  await app.listen(config.api.port);
+  await app.listen(3000);
 }
 bootstrap();
--- apps/wallet-processor/src/main.ts
+++ apps/wallet-processor/src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { WalletProcessorModule } from './wallet-processor.module';
-import { SharedConfigService } from '@app/shared-config';
 
 async function bootstrap() {
-  const configService = new SharedConfigService();
-  const config = configService.getConfig();
   const app = await NestFactory.create(WalletProcessorModule);
-  await app.listen(config.processor.port);
+  await app.listen(3001);
 }
 bootstrap();
